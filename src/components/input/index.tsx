f1c12e3e5109b28760716728791ea0535b27928d
Daniel Belarmino <belarba@gmail.com>
2020-06-25 14:57:17 -0900

    form validation

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -65,7 +65,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProprs> = ({ name, icon
     });
   }, [fieldName, registerField])
 
-  return (<Container isFocused={isFocused}>
+  return (<Container isFocused={isFocused} isErrored={!!error}>
     <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
     <TextInput
       ref={inputElementRef}
