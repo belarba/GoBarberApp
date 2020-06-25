d31d0d964f00af8eb704382f2e93fe8ef548a809
Daniel Belarmino <belarba@gmail.com>
2020-06-25 11:39:08 -0900

    usability in forms

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import {
   View,
   ScrollView,
   KeyboardAvoidingView,
-  Platform
+  Platform,
+  TextInput
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -27,6 +28,7 @@ import {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
   const handleSignIn = useCallback((data: object) => {
@@ -45,20 +47,37 @@ const SignIn: React.FC = () => {
             <Image source={logoImg} />
             <View><Title>Faça seu logon</Title></View>
             <Form ref={formRef} onSubmit={handleSignIn}>
-              <Input name='email' icon='mail' placeholder='E-mail' />
-              <Input name='password' icon='lock' placeholder='Senha' />
-              <Button onPress={() => formRef.current?.submitForm(); } >Entrar</Button>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name='email'
+                icon='mail'
+                placeholder='E-mail'
+                returnKeyType="next"
+                onSubmitEditing={() => { passwordInputRef.current?.focus(); }}
+              />
+              <Input
+                ref={passwordInputRef}
+                name='password'
+                icon='lock'
+                placeholder='Senha'
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+              <Button onPress={() => formRef.current?.submitForm()} >Entrar</Button>
             </Form>
-          <ForgotPassword onPress={() => { console.log('press'); }}>
-            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-          </ForgotPassword>
+            <ForgotPassword onPress={() => { console.log('press'); }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
           </Container>
         </ScrollView>
-    </KeyboardAvoidingView>
-    <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
-      <Icon name="log-in" size={20} color='#ff9000' />
-      <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
-    </CreateAccountButton>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color='#ff9000' />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
     </>
   );
 }
